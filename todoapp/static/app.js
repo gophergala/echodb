6d773d38928c130999e3e8181574a78b66fc959b
Michal Ostrowski <ostrowski.michal@gmail.com>
2015-01-25 17:58:56 +0100

    use websockets

--- todoapp/static/app.js
+++ todoapp/static/app.js
@@ -9,4 +9,47 @@ $(function () {
 
   // kick things off by creating the `App`
   new app.AppView();
+
+  // if a browser supports websocket we're going to use it :)
+  if (window["WebSocket"]) {
+    var conn = new WebSocket("ws://" + url + "/ws/todo");
+
+    conn.onclose = function(evt) {
+    }
+
+    conn.onmessage = function(evt) {
+      var data = {};
+
+      try {
+        data = JSON.parse(evt.data)
+        if(data.__action) {
+          data.__doc.id = data.__doc._id;
+          if(data.__action === "create") {
+            app.todos.add(data.__doc)
+          }
+          else if(data.__action === "update") {
+            var model = app.todos.get({id: data.__doc._id})
+            if(model) {
+              model.set('title', data.__doc.title)
+              model.set('completed', data.__doc.completed)
+              model.set('order', data.__doc.order)
+            }
+          }
+          else if(data.__action === "delete") {
+            var model = app.todos.get({id: data.__doc._id})
+            if(model) {
+              model.url = function() {}
+              model.destroy()
+            }
+          }
+        }
+      }
+      catch (e) {
+      }
+    }
+    conn.onerror = function(err) {
+    }
+    conn.onopen = function() {
+    }
+  }
 });
